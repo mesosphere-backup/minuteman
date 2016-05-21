8f6e6a3ce3dd1aedaebc7282218c44acd3b186d4
Sargun Dhillon <sargun@sargun.me>
2016-05-21 01:22:22 -0700

    Name based VIPs   -Adding parsing logic to minuteman_mesos_poller for name based VIPs   -Adds IP conversion functions   -DNS integration (Spartan) for name based VIPs   -Add dns / zone test   -Upgrade rebar3   -Upgrade circle-ci environment to erlang 18.3   -Add logic to load override config for DC/OS   -Add mesos_state to minuteman runtime dependencies   -Make sure dns application is started

--- src/minuteman_app.erl
+++ src/minuteman_app.erl
@@ -5,14 +5,32 @@
 %% Application callbacks
 -export([start/2, stop/1]).
 
+-define(DEFAULT_CONFIG_LOCATION, "/opt/mesosphere/etc/minuteman.app.config").
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+    load_config(),
     {ok, _} = application:ensure_all_started(exometer_core),
     minuteman_metrics:setup(),
     minuteman_sup:start_link().
 
 stop(_State) ->
     ok.
+
+load_config() ->
+    case file:consult(?DEFAULT_CONFIG_LOCATION) of
+        {ok, Result} ->
+            load_config(Result),
+            lager:info("Loaded config: ~p", [?DEFAULT_CONFIG_LOCATION]);
+        {error, enoent} ->
+            lager:info("Did not load config: ~p", [?DEFAULT_CONFIG_LOCATION])
+    end.
+
+load_config([Result = [_]]) ->
+    lists:foreach(fun load_app_config/1, Result).
+
+load_app_config({App, Options}) ->
+    lists:foreach(fun({OptionKey, OptionValue}) -> application:set_env(App, OptionKey, OptionValue) end, Options).
+
