9f915dc75b58543103a50df725285352b37ff9fa
Anatoly Yakovenko <aeyakovenko@gmail.com>
2016-09-14 14:49:22 -0700

    fixed socket leak

---
+++ test/minuteman_restart.erl
@@ -0,0 +1,18 @@
+-module(minuteman_restart).
+-compile(export_all).
+
+-include_lib("common_test/include/ct.hrl").
+
+all() ->
+  [test_init
+  ,test_init
+  ].
+
+test_init(_Config) -> ok.
+
+init_per_testcase(_, Config) ->
+  {ok, _} = application:ensure_all_started(minuteman),
+  Config.
+
+end_per_testcase(_, _Config) -> 
+  ok = application:stop(minuteman).
