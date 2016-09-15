d14d6163213f7a07ac899b502257018019baf853
Anatoly Yakovenko <aeyakovenko@gmail.com>
2016-09-15 10:45:15 -0700

    split out root and non root tests

--- test/minuteman_restart_SUITE.erl
+++ test/minuteman_restart_SUITE.erl
@@ -3,7 +3,13 @@
 
 -include_lib("common_test/include/ct.hrl").
 
-all() -> [test_restart].
+all() -> all(os:cmd("id -u")).
+
+% tests that need root
+all("0\n") -> [test_restart].
+
+% tests that do not need root
+all(_) -> [].
 
 test_restart(_Config) ->
   {ok, _} = application:ensure_all_started(minuteman),
