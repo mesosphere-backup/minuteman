309c6655a1c9aa79921df0696bec2a13da2586d5
Sargun Dhillon <sargun@sargun.me>
2016-10-13 14:24:21 -0700

    Make sure procket doesn't get compiled on non-Linux

---
+++ src/minuteman_netlink.erl
@@ -0,0 +1,13 @@
+%%%-------------------------------------------------------------------
+%%% @author sdhillon
+%%% @copyright (C) 2016, Mesosphere, Inc.
+%%% @doc
+%%%
+%%% @end
+%%% Created : 10. Oct 2016 5:16 PM
+%%%-------------------------------------------------------------------
+-module(minuteman_netlink).
+-author("sdhillon").
+
+%% API
+-export([]).
