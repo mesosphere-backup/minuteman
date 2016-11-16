e3eb8ccb036a3d3ba536907b33bc2f9a2d94b70f
Anatoly Yakovenko <aeyakovenko@gmail.com>
2016-11-15 21:28:16 -0800

    check previous backends as well, elvis

--- src/minuteman_metrics.erl
+++ src/minuteman_metrics.erl
@@ -23,8 +23,11 @@
 
 -define(SERVER, ?MODULE).
 
+-type backend_conns() :: #{inet:ip4_address() => [#ip_vs_conn{}]}.
+
 -record(state, {
-          conns = maps:new() :: conn_map()
+          conns = maps:new() :: conn_map(),
+          backend_conns = maps:new() :: backend_conns()
     }).
 
 -spec(start_link() ->
@@ -64,9 +67,9 @@ handle_cast(_Request, State) ->
     {noreply, NewState :: #state{}, timeout() | hibernate} |
     {stop, Reason :: term(), NewState :: #state{}}).
 handle_info(push_metrics, State = #state{}) ->
-    NewConns = check_connections(State#state.conns),
+    {NewConns, NewBCs} = check_connections(State#state.conns, State#state.backend_conns),
     erlang:send_after(splay_ms(), self(), push_metrics),
-    {noreply, State#state{conns = NewConns}};
+    {noreply, State#state{conns = NewConns, backend_conns = NewBCs}};
 handle_info(_Info, State) ->
     {noreply, State}.
 
@@ -92,20 +95,22 @@ splay_ms() ->
     NextMinute + Splay.
 
 %% implementation
--spec(check_connections(conn_map()) -> conn_map()).
-check_connections(OldConns) ->
+-spec(check_connections(conn_map(), backend_conns()) -> {conn_map(), backend_conns()}).
+check_connections(OldConns, OldParsedMap) ->
     {ok, Conns} = ip_vs_conn_monitor:get_connections(),
     Splay = minuteman_config:metrics_splay_seconds(),
     Interval = minuteman_config:metrics_interval_seconds(),
     PollDelay = 2*(Splay + Interval), %% assuming these delays are the same in ip_vs_conn
     OnlyNewConns = new_connections(OldConns, Conns),
     Parsed = lists:map(fun ip_vs_conn:parse/1, maps:to_list(OnlyNewConns)),
-    lists:foreach(fun (C) -> check_connections(C, PollDelay) end, Parsed),
+    lists:foreach(fun (C) -> apply_connection(C, PollDelay) end, Parsed),
     ParsedMap = lists:foldl(fun vip_addr_map/2, #{}, Parsed),
     Metrics = tcp_metrics_monitor:get_metrics(),
     P99s = get_p99s(ParsedMap, Metrics),
     lists:foreach(fun apply_p99/1, P99s),
-    Conns.
+    OldP99s = get_p99s(OldParsedMap, Metrics),
+    lists:foreach(fun apply_p99/1, OldP99s),
+    {Conns, ParsedMap}.
 
 vip_addr_map(C = #ip_vs_conn{dst_ip = IP}, Z) ->
     vip_addr_map(C, Z, maps:get(int_to_ip(IP), Z, undefined)).
@@ -116,22 +121,22 @@ vip_addr_map(C = #ip_vs_conn{dst_ip = IP}, Z, Ls) ->
     maps:put(int_to_ip(IP), [C | Ls], Z).
 
 new_connections(OldConns, AllConns) ->
-    maps:filter(fun(K,V) -> has_new_state(OldConns, {K,V}) end, AllConns).
+    maps:filter(fun(K, V) -> has_new_state(OldConns, {K, V}) end, AllConns).
 
 has_new_state(_OldConns, {_K, #ip_vs_conn_status{tcp_state = syn_recv}}) -> true;
 has_new_state(_OldConns, {_K, #ip_vs_conn_status{tcp_state = syn_sent}}) -> true;
 has_new_state(OldConns, {K, _V}) -> not(maps:is_key(K, OldConns)).
 
--spec(check_connections(#ip_vs_conn{}, integer()) -> ok).
-check_connections(Conn = #ip_vs_conn{expires = Expires, tcp_state = syn_recv},
+-spec(apply_connection(#ip_vs_conn{}, integer()) -> ok).
+apply_connection(Conn = #ip_vs_conn{expires = Expires, tcp_state = syn_recv},
                   PollDelay) when Expires < PollDelay ->
     conn_failed(Conn);
-check_connections(Conn = #ip_vs_conn{expires = Expires, tcp_state = syn_sent},
+apply_connection(Conn = #ip_vs_conn{expires = Expires, tcp_state = syn_sent},
                   PollDelay) when Expires < PollDelay ->
     conn_failed(Conn);
-check_connections(#ip_vs_conn{tcp_state = syn_recv}, _PollDelay) -> ok;
-check_connections(#ip_vs_conn{tcp_state = syn_sent}, _PollDelay) -> ok;
-check_connections(Conn, _PollDelay) ->
+apply_connection(#ip_vs_conn{tcp_state = syn_recv}, _PollDelay) -> ok;
+apply_connection(#ip_vs_conn{tcp_state = syn_sent}, _PollDelay) -> ok;
+apply_connection(Conn, _PollDelay) ->
     conn_success(Conn).
 
 -spec(conn_failed(#ip_vs_conn{}) -> ok).
@@ -151,13 +156,13 @@ conn_success(#ip_vs_conn{dst_ip = IP, dst_port = Port,
 get_p99s(Conns, Metrics) ->
     lists:flatmap(fun (M) -> get_p99_updates(Conns, M) end, Metrics).
 
-get_p99_updates(Conns, {netlink,tcp_metrics,_, _, _, {get,_,_,Attrs}}) ->
+get_p99_updates(Conns, {netlink, tcp_metrics, _, _, _, {get, _, _, Attrs}}) ->
     match_metrics(Conns, proplists:get_value(d_addr, Attrs), proplists:get_value(vals, Attrs)).
 
 match_metrics(_, undefined, _) -> [];
 match_metrics(_, _, undefined) -> [];
 match_metrics(Conns, Addr, Vals) -> match_conn(maps:get(Addr, Conns, undefined),
-                                               proplists:get_value(rtt_us, Vals), 
+                                               proplists:get_value(rtt_us, Vals),
                                                proplists:get_value(rtt_var_us, Vals)).
 match_conn(undefined, _, _) -> [];
 match_conn(_, undefined, _) -> [];
@@ -215,20 +220,20 @@ new_conn1_test_() ->
     [?_assertEqual(#{Conn => ConnStatus1}, New)].
 
 get_p99s_test_() ->
-    DAddr = {54,192,147,29},
-    Attrs = [{d_addr,DAddr},
-             {s_addr,{10,0,79,182}},
-             {age_ms,806101408},
-             {vals,[{rtt_us,47313},
-                    {rtt_ms,47},
-                    {rtt_var_us,23656},
-                    {rtt_var_ms,23},
-                    {cwnd,10}]}],
-    Metrics = [{netlink,tcp_metrics, [multi], 18,31595, {get,1,0,Attrs}}],
+    DAddr = {54, 192, 147, 29},
+    Attrs = [{d_addr, DAddr},
+             {s_addr, {10, 0, 79, 182}},
+             {age_ms, 806101408},
+             {vals, [{rtt_us, 47313},
+                    {rtt_ms, 47},
+                    {rtt_var_us, 23656},
+                    {rtt_var_ms, 23},
+                    {cwnd, 10}]}],
+    Metrics = [{netlink, tcp_metrics, [multi], 18, 31595, {get, 1, 0, Attrs}}],
     IP = 16#36c0931d,
-    Conn = {ip_vs_conn,tcp,established,167792566,47808,167792566,8080,IP,8081,59},
-    Conn2 = {ip_vs_conn,tcp,established,167792567,47808,167792566,8080,IP,8081,59},
-    DAddr = int_to_ip(IP), 
+    Conn = {ip_vs_conn, tcp, established, 167792566, 47808, 167792566, 8080, IP, 8081, 59},
+    Conn2 = {ip_vs_conn, tcp, established, 167792567, 47808, 167792566, 8080, IP, 8081, 59},
+    DAddr = int_to_ip(IP),
     ConnMap = #{DAddr => [Conn]},
     ConnMap2 = #{DAddr => [Conn, Conn2]},
     [?_assertEqual(DAddr, int_to_ip(IP)),
