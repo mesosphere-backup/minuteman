bf8fd3a378cc4d51993f47f6401f5fc6734893df
Deepak Goel <dgoel@mesosphere.com>
2017-02-15 11:45:19 -0800

    Adds lashup and dets value check

--- test/minuteman_mesos_poller_SUITE.erl
+++ test/minuteman_mesos_poller_SUITE.erl
@@ -17,13 +17,20 @@ test_gen_server(_Config) ->
     sys:change_code(minuteman_mesos_poller, random_old_vsn, minuteman_mesos_poller, []),
     sys:resume(minuteman_mesos_poller).
 
-test_handle_poll_state(_Config) ->
-    {ok, Data} = file:read_file("../../../../testdata/missing-port.json"),
+test_handle_poll_state(Config) ->
+    AgentIP = {1, 1, 1, 1},
+    DataDir = ?config(data_dir, Config),
+    io:format("DataDir ~p~n", [DataDir]),
+    {ok, Data} = file:read_file(filename:join(DataDir, "named-base-vips.json")),
     {ok, MesosState} = mesos_state_client:parse_response(Data),
-    State0 = {state, {0, 0, 0, 0}, ordsets:new(), 0},
+    State0 = {state, AgentIP, ordsets:new(), 0},
     State1 = minuteman_mesos_poller:handle_poll_state(MesosState, State0),
-    %% TODO check lashup and dets
-    {state, {0, 0, 0, 0}, [], _} = State1.
+    LashupValue = lashup_kv:value([minuteman, vips]),
+    [{_, [{{10,0,0,243}, 12049}]}] = LashupValue,
+    {ok, Name} = dets:open_file(minuteman_config:agent_dets_path("agent_be"), []),
+    DetsValue = dets:lookup(Name, AgentIP),
+    [{AgentIP, [{10,0,0,243}]}] = DetsValue,
+    {state, AgentIP, [{10,0,0,243}], _} = State1.
 
 init_per_testcase(_, Config) ->
   PrivateDir = ?config(priv_dir, Config),
